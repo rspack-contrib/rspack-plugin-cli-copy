502eca88e6ead08184eec5047d1728c0e155d6ad
wChenonly <wChenonly@qq.com>
2024-07-09 09:32:40 +0800

    chore: optimize dev server startup in rspackCliCopyPlugin

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,18 @@ const clipboard = new Clipboard()
 export class rspackCliCopyPlugin implements RspackPluginInstance {
   apply(compiler: Compiler) {
     compiler.hooks.afterCompile.tap('NetworkCopyPlugin', async () => {
-      const localIPv4 = (await WebpackDevServer.internalIP('v4')) || ''
-      const port = compiler.options.devServer?.port
-      const v = `http://${localIPv4}:${port}`
-      print(v)
+      if (compiler.options.mode === 'development' && !compiler.modifiedFiles?.size) {
+        const localIPv4 = (await WebpackDevServer.internalIP('v4')) || ''
+        const port = compiler.options.devServer?.port
+        const v = `http://${localIPv4}:${port}`
+        print(v)
+      }
     })
   }
 }
 
 export const rsbuildCliCopyPlugin = (): RsbuildPlugin => ({
   name: 'rsbuildCliCopyPlugin',
-
   async setup(api) {
     const localIPv4 = (await WebpackDevServer.internalIP('v4')) || ''
     api.onAfterStartDevServer(server => {
